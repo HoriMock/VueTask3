2835631f405484dccfc737f4c1305e60e4613335
hori <hori.mock11@gmail.com>
2021-12-06 14:13:39 +0900

    Vue課題③の実装

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script>
+import TodoList from "./components/TodoList.vue";
+
 export default {
   name: "App",
+  components: {
+    TodoList,
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 </style>
